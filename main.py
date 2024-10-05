38dd9a0d796221efddeeab95b14865bef5e1c374
JasonXing1234 <Jason.Xing@cat.com>
2024-10-04 22:37:22 -0600

    output.txt

--- main.py
+++ main.py
@@ -13,10 +13,23 @@ def main(prompt_file):
         os.remove('taxi.db')
     database = Database('taxi.db')
     database.run_script('./sqlite/init.sql')
+
     query = takingPrompt(compile_prompt(prompt_file_name = prompt_file))
-    print(takingPrompt(compile_prompt('./prompts/init_response.txt', prompt_file) + "\nHere is the answer:" + str(database.execute(query))))
-    with open('out.txt', 'w') as f:
-        print('Filename:', 'output.txt', file=f)
+    print(compile_prompt(prompt_file_name = prompt_file))
+    print(query)
+    tempReponse = takingPrompt(compile_prompt('./prompts/init_response.txt', prompt_file) + "\nHere is the answer:" + str(database.execute(query)))
+    with open('out.txt', 'a') as f:
+       with open('./prompts/init.txt', 'r') as i:
+        with open('./prompts/init_response.txt', 'r') as p:
+            with open(prompt_file, 'r') as prompt:
+                print("\n\nConversation", file=f)
+                print(prompt.read(), file=f)
+                print(query, file=f)
+                print(compile_prompt('./prompts/init_response.txt', prompt_file), file=f)
+                print(tempReponse, file=f)
+            
+
+            
 
 
 def test():
